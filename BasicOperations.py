7000892cc90c6dd15e7251382f32081dba76a3ac
Marco Elz <Marco Elz>
2016-02-26 15:11:15 -0600

    BasicOperations.py added - toGrayScale method added

---
+++ BasicOperations.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+#Vuelve la imagen en escala de grises
+def toGrayScale(pixeles, ancho, alto):
+	for i in range(ancho):
+	    for j in range(alto):
+	    	pixel = pixeles[i,j]
+	    	newpx = int(pixel[0]*0.299)+int(pixel[1]*0.587)+int(pixel[2]*0.114)   
+	    	pixeles[i,j] = (newpx, newpx, newpx)
+	return pixeles
+
+
+#En prueba
+def normalize(pixeles, max, ancho, alto):
+	for i in range(ancho):
+	    for j in range(alto):
+	    	pixel = pixeles[i,j]
+	    	newpx = int((pixel[0] / max) * 255)
+	    	pixeles[i,j] = (newpx, newpx, newpx)
+	return pixeles
+
