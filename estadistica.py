71c0d0dcfad4626d69de19852bae823405acaacc
MarcoElz <marco.eelizondo@gmail.com>
2016-03-04 10:59:22 -0600

    Modifique estadistica.py para que sea compatible con las imagenes

--- estadistica.py
+++ estadistica.py
@@ -1,13 +1,15 @@
 
-def average(lista):
+def average(pixeles, ancho, alto):
 	suma = 0
-	for value in lista:
-		suma += value
-	return suma / len(lista)
+	for i in range(ancho):
+		    for j in range(alto):
+		    	suma += pixeles[i,j][0]
+	return suma / (ancho * alto)
 
-def varianza(lista):
-	avera = average(lista)
+def varianza(pixeles, ancho, alto, avera):
+	#avera = average(pixeles, ancho, alto)
 	suma = 0
-	for value in lista:
-		suma += (value - avera)**2
-	return suma / len(lista)
+	for i in range(ancho):
+		    for j in range(alto):
+		    	suma += (pixeles[i,j][0] - avera)**2
+	return suma / (ancho * alto)
