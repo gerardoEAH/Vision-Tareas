02a3a8b094df50493959abcf1c321585e3728fad
MarcoElz <marco.eelizondo@gmail.com>
2016-03-06 23:07:03 -0600

    Añadidos mensajes a programas

--- AclararImagen.py
+++ AclararImagen.py
@@ -2,35 +2,35 @@ import BasicOperations as bo;
 from PIL import Image
 from sys import argv
 
-#obtiene nombre de imagen como parametro
-nombreImagen = argv[1]
 
-#Selecciona el brillo a agregar
-x = -1;
-while x < 0:
-	x = input("Ingresa la cantidad a aclarar (mayor a 0.0) : ")
+def aclararImagen():
 
-#Obtiene la imagen,el tamano, y los pixeles
-img = Image.open(nombreImagen)
-ancho,alto = img.size
-pixeles = img.load()
+	#obtiene nombre de imagen como parametro
+	nombreImagen = argv[1]
 
-#Los pixeles a color los cambia a Escala de Grises
-bo.toGrayScale(pixeles, ancho, alto)
-max = 0
+	#Selecciona el brillo a agregar
+	x = -1;
+	while x < 0:
+		x = input("Ingresa la cantidad a aclarar (mayor a 0.0) : ")
 
-#Aumenta el brillo a la imagen
-for i in range(ancho):
-	    for j in range(alto):
-	    	pixel = pixeles[i,j]
-	    	newpx = int(pixel[0] * x)    	
-	    	pixeles[i,j] = (newpx, newpx, newpx)
-	    	if newpx > max :
-	    		max = newpx
+	#Obtiene la imagen,el tamano, y los pixeles
+	img = Image.open(nombreImagen)
+	ancho,alto = img.size
+	pixeles = img.load()
 
-#Normaliza la imagen para que un pixel no sobrepase los 255
-#if max > 255:
-#	bo.normalize(pixeles, max, ancho, alto)
+	#Los pixeles a color los cambia a Escala de Grises
+	bo.toGrayScale(pixeles, ancho, alto)
+	max = 0
 
-new = "aclaradoDeImagen.jpg"
-img.save(new)
+	#Aumenta el brillo a la imagen
+	for i in range(ancho):
+		    for j in range(alto):
+		    	pixel = pixeles[i,j]
+		    	newpx = int(pixel[0] * x)    	
+		    	pixeles[i,j] = (newpx, newpx, newpx)
+		    	if newpx > max :
+		    		max = newpx
+
+	new = "aclaradoDeImagen.jpg"
+	img.save(new)
+	print "Imagen generada con exito 'aclaradoDeImagen.jpg'"
--- Umbral.py
+++ Umbral.py
@@ -2,31 +2,34 @@ import BasicOperations as bo;
 from PIL import Image
 from sys import argv
 
-#obtiene nombre de imagen como parametro
-nombreImagen = argv[1]
+def umbral():
 
-#Selecciona el umbral
-thresh = 0;
-while thresh < 1 or thresh > 255:
-	thresh = input("Ingresa el umbral (1-255) : ")
+	#obtiene nombre de imagen como parametro
+	nombreImagen = argv[1]
 
-#Obtiene la imagen,el tamano, y los pixeles
-img = Image.open(nombreImagen)
-ancho,alto = img.size
-pixeles = img.load()
+	#Selecciona el umbral
+	thresh = 0;
+	while thresh < 1 or thresh > 255:
+		thresh = input("Ingresa el umbral (1-255) : ")
 
-#Los pixeles a color los cambia a Escala de Grises
-bo.toGrayScale(pixeles, ancho, alto)
+	#Obtiene la imagen,el tamano, y los pixeles
+	img = Image.open(nombreImagen)
+	ancho,alto = img.size
+	pixeles = img.load()
 
-#Umbral Blanco-Negro
-for i in range(ancho):
-	    for j in range(alto):
-	    	pixel = pixeles[i,j]
-	    	if pixel[0] > thresh:
-	    		newpx = 255
-	    	else:
-	    		newpx = 0  	
-	    	pixeles[i,j] = (newpx, newpx, newpx)
+	#Los pixeles a color los cambia a Escala de Grises
+	bo.toGrayScale(pixeles, ancho, alto)
 
-new = "umbral.jpg"
-img.save(new)
+	#Umbral Blanco-Negro
+	for i in range(ancho):
+		    for j in range(alto):
+		    	pixel = pixeles[i,j]
+		    	if pixel[0] > thresh:
+		    		newpx = 255
+		    	else:
+		    		newpx = 0  	
+		    	pixeles[i,j] = (newpx, newpx, newpx)
+
+	new = "umbral.jpg"
+	img.save(new)
+	print "Imagen generada con exito 'umbral.jpg'"
