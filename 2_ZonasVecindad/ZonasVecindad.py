171602f2f1005310b59fbb2c8b99d1c36778ed07
MarcoElz <marco.eelizondo@gmail.com>
2016-03-30 14:22:56 -0600

    Version 1 de Zonas de Vecindad terminado. El programa guarda un txt con una matriz con la zona de cada pixel. Se recomienda optimizar

--- 2_ZonasVecindad/ZonasVecindad.py
+++ 2_ZonasVecindad/ZonasVecindad.py
@@ -1,7 +1,9 @@
+#Este codigo es funcional. Pero en mi opinion le falta optimizarse mas.
+#Fue lo que realice un poco a prisas y es la version inicial.
+
 import numpy as np
 from sys import argv
 import cv2
-
 
 #obtiene nombre de imagen como parametro
 nombreImagen = argv[1]
@@ -29,23 +31,25 @@ for i in range(ancho):
 cv2.imwrite(nombreImagen + 'TONOS.jpg', img)
 print "Guardando imagen de tonos"
 
+
 ###### Zonas de Vecindad ######
 #Crear array de Zonas donde el valor de todo es 0
 zones = np.zeros(img.shape, dtype=np.int)
-np.savetxt('zonas1.txt', zones, delimiter='-') 
 
-###
+##Funciones que se usaran##
 
-
-##
-###
-#FUNCIONES
-
+#Esta funcion mira los vecinos del pixel (i, j) y si estos estan dentro del rango los une a a la misma zona.
 def vecinos(i, j):
 
+	#Variables globales que se deben usar
 	global value
 	global n
 	global total
+
+	#Se verifica que el pixel adyacente se encuentre en los rangos de la imagen.
+	#Despues se checa que no tengo zona, es decir su zona sea 0
+	#Por ultimo se checa si la diferencia de intensidades este dentro del rango.
+	#Y si es asi, lo coloca dentro de la zona 'n'. Y disminuimos en uno el total de pixeles que faltan de unir a una zona	
 
 	if i - 1 >= 0:
 		if zones[i - 1, j] == 0:
@@ -71,8 +75,15 @@ def vecinos(i, j):
 				zones[i,j + 1] = n
 				total -= 1
 
+#Esta funcion nos regresa verdadero si alguno de los vecinos del pixel (i,j) pertecen a la zona 'n' que estamos trabajando
+#De lo contrario nos regresa falso
 def isVecino(i, j):
+	
+	#Variable global de la zona
 	global n
+
+	#Se verifica que el pixel que queremos ver exista dentro de los limites de la imagen
+	#Despues se checa si pertenecen a la zona y si es asi nos regresa verdadero
 
 	if i - 1 >= 0:
 		if zones[i - 1, j] == n:
@@ -90,196 +101,66 @@ def isVecino(i, j):
 		if zones[i, j + 1] == n:
 			return True
 
+	#Si no regresa verdadero porque no tiene vecinos de la zona nos regresa falso.
 	return False
 
-##3
-#Itera por la imagen
 
-#La zona. Se inicia en 1 y cuando termine de encontrar una zona se incrementa para la siguiente zona
-n = 1
+#Comienza a correr el algoritmo
 
-#Declarar variables y seleccionar el pixel (0,0)
+#Declarar variables y seleccionar el pixel inicial como el  (0,0)
+n = 1 #La zona. Se inicia en 1 y cuando termine de encontrar una zona se incrementa para la siguiente zona
+i = 0
+j = 0
+value = img.item(i,j) 	#Valor del pixel con el cual comparar los demas
+zones[i,j] = n		#Pixel inicial esta en la primera zona
+selected = True		#Ya se selecciono un pixel y zona asi que es verdadero
+i += 1			#Nos movemos al pixel adyacente
+total = ancho * alto 	#Total de pixeles
 
-px = 0
-py = 0
-i = px
-j = py
-value = img.item(px,py)
-zones[px,py] = n
-selected = True
-i += 1
-total = ancho * alto
 
-while total > 10:
-
-	while j <= alto:
+#Iterara hasta que no haya pixeles sin zona
+while total > 1:
+	#Detiene este ciclo cuando recorra toda la imagen
+	while j <= alto: 
+		#Que no se pase del limite de la imagen
 		if i + 1 <= ancho and j + 1 <= alto:
+			#Si el pixel no tiene zona y no hay seleccion de nueva zona.
 			if zones[i,j] == 0 and not(selected):
-				n += 1
-				value = img.item(i,j)
-				zones[i,j] = n
-				total -= 1
-				selected = True
-				print "Total: " + str(total)
-				print "Zona: " + str(n)
-				np.savetxt('zonas.txt', zones, fmt='%1s', delimiter='-') 
+				n += 1			#Incrementa n para empezar con la siguiente zona
+				value = img.item(i,j)	#Guarda el valor del pixel actual para las comparaciones
+				zones[i,j] = n		#Vuelve el pixel actual en la nueva zona
+				total -= 1		#Al tener un pixel nuevo en zona, disminuimos total para saber cuantos pixeles faltan
+				selected = True		#Como seleccionamos una nueva zona volvemos selected verdadero
 
+			#Si no tiene zona, pero si se ha seleccionado una nueva zona
 			elif zones[i,j] == 0 and selected:
-				###
+				#Si tiene vecinos con zona actual
 				if isVecino(i,j):
+					#Si entra en el rango para pertenecer a la zona
 					if (value - img.item(i,j))**2 < thresh:
-						zones[i,j] = n
-						total -= 1
-						vecinos(i,j)
+						zones[i,j] = n	#Vuelve el pixel actual en la zona 
+						total -= 1	#Al tener un pixel nuevo en zona, disminuimos total para saber cuantos pixeles faltan
+						vecinos(i,j)	#Checa si sus vecinos pertenecen a la zona.
 			
-				
+			#Si la zona del pixel actual pertenece ya a la zona, checa sus vecinos para ver si tambien pertenecen
 			elif zones[i,j] == n:
 				vecinos(i,j)
 
+		#Nos movemos en la imagen
 		if i + 1 < ancho:
 			i += 1
 		else:
 			i = 0
 			j += 1
-	selected = False
-	i = 0
+
+	#Al teminar de ver toda la imagen y llenamos la zona, proseguimos a repetir el proceso con una nueva zona.
+	selected = False	#No tenemos pixel seleccionado asi que toma el valor de falso
+	i = 0			#Volvemos a movernos por la imagen desde el inicio
 	j = 0
 
-"""
-	if zones(i,j) == 0 and not(selected):
-		px = i
-		py = j
-		value = img.item(i,j)
-		zones(i, j) = n
-		selected = true
-		if i + 1 < ancho:
-		i += 1
-		else:
-		j += 1
 
-	elif zones(i,j) == 0 and selected:
-		if (value - img.item(i,j))**2 < thresh:
-			zones(i,j) = n
-			
-	
-		if i + 1 <= ancho:
-			i += 1
-		else:
-			i = px
-			j += 1
-
-	else:
-		selected = false
-		i = 0
-		j = 0
-
-"""
-
-
-#########INTENTOS
-"""
-#Declarar variables y seleccionar el pixel (0,0)
-px = 0
-py = 0
-i = px
-j = py
-value = img.item(px,py)
-zones(px,py) = n
-selected = true
-i += 1
-"""
-
-###
-
-
-"""
-#Itera por la imagen
-while j <= alto:
-	if zones(i,j) == 0 and not(selected):
-		px = i
-		py = j
-		value = img.item(i,j)
-		zones(i, j) = n
-		selected = true
-		if i + 1 < ancho:
-		i += 1
-		else:
-		j += 1
-
-	elif zones(i,j) == 0 and selected:
-		if (value - img.item(i,j))**2 < thresh:
-			zones(i,j) = n
-			
-	
-		if i + 1 <= ancho:
-			i += 1
-		else:
-			i = px
-			j += 1
-
-	else:
-		selected = false
-		i = 0
-		j = 0
-"""
-###
-
-"""
-n = 1
-value = img.item(0,0)
-
-def vecinos(i, j):
-
-	global value
-	global n
-
-	if i - 1 >= 0 and zones[i - 1, j] == 0:
-		if (value - img.item(i,j))**2 < thresh:
-			zones[i - 1,j] = n
-			vecinos(i - 1, j)
-
-	if j - 1 >= 0 and zones[i,j - 1] == 0:
-		if (value - img.item(i,j))**2 < thresh:
-			zones[i,j - 1] = n
-			vecinos(i, j - 1)
-
-	if i + 1 <= ancho and zones[i + 1,j] == 0:
-		if (value - img.item(i,j))**2 < thresh:
-			zones[i + 1,j] = n
-			vecinos(i + 1, j)
-
-	if j + 1 <= alto and zones[i,j + 1] == 0:
-		if (value - img.item(i,j))**2 < thresh:
-			zones[i,j + 1] = n
-			vecinos(i, j + 1)
-
-
-	if i + 1 <= ancho and j + 1 <= alto:
-		next = True
-		while next:
-			if i + 1 < ancho:
-				i += 1
-			else:
-				i = 0
-				j += 1
-			
-			if j > alto:
-				next = False
-				print "FIN"
-
-			if zones[i,j] == 0:
-				next = False
-				value = img.item(i,j)
-				n += 1
-				vecinos(i, j)
-
-
-
-
-vecinos(0,0)
-"""
-
-
+#OPCIONAL
+#Guargamos el archivo de la matrix en un archivo de texto, donde cada pixel es el numero de zona y estan divididos por '-'
 np.savetxt('zonas.txt', zones, fmt='%1s', delimiter='-') 
 print "Text File saved."
 
